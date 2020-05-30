f4613e2bf1137b368ebae9f790363e29f1510b9b
佐藤周平 <s.s.odn.0617@gmail.com>
2020-05-31 03:25:38 +0900

    learn component

--- main.js
+++ main.js
@@ -50,3 +50,19 @@ var app6 = new Vue({
     message: 'Hello Vue!'
   }
 });
+
+Vue.component('todo-item', {
+  props: ['todo'],
+  template: '<li>{{ todo.text }}</li>'
+});
+
+var app7 = new Vue({
+  el: '#app-7',
+  data: {
+    groceryList: [
+      { id: 0, text: 'apple' },
+      { id: 1, text: 'orange' },
+      { id: 2, text: 'lemon' }
+    ]
+  }
+});
