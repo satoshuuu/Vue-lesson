06074e21be9b9d725e3d2196b2f18c592cbdc07d
佐藤周平 <s.s.odn.0617@gmail.com>
2020-05-30 15:51:23 +0900

    learn if

--- main.js
+++ main.js
@@ -11,3 +11,11 @@ var app2 = new Vue({
     message: 'You loaded this page on' + new Date().toLocaleString()
   }
 });
+
+var app3 = new Vue({
+  el: '#app-3',
+  data: {
+    seen: true
+  }
+})
+app3.seen = false;
