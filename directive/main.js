89702c7c0d80a005dbac02c21fafe714dc50e92d
佐藤周平 <s.s.odn.0617@gmail.com>
2020-06-11 22:20:04 +0900

    add directive

---
+++ directive/main.js
@@ -0,0 +1,34 @@
+let app = new Vue({
+  el: "#app",
+  data: {
+    message: "こんにちは",
+  }
+});
+
+let app2 = new Vue({
+  el: "#app2",
+  data: {
+    message: "おはようございます",
+  }
+});
+
+let app3 = new Vue({
+  el: "#app3",
+  data: {
+    url: "https://jp.vuejs.org/index.html",
+  }
+});
+
+let app4 = new Vue({
+  el: "#app4",
+  data: {
+    url: "https://jp.vuejs.org/index.html",
+  }
+});
+
+let app5 = new Vue({
+  el: "#app5",
+  data: {
+    flag: true,
+  }
+});
