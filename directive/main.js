d8cb1d15144a8a428ae14992e7d7cac3192e74b2
佐藤周平 <s.s.odn.0617@gmail.com>
2020-06-15 18:42:41 +0900

    add v-on mousemove example

--- directive/main.js
+++ directive/main.js
@@ -44,3 +44,29 @@ let app6 = new Vue({
     }
   }
 })
+
+let app7 = new Vue({
+  el: "#app7",
+  data: {
+    screenX: 0,
+    screenY: 0,
+    pageX: 0,
+    pageY: 0,
+    clientX: 0,
+    clientY: 0,
+    offsetX: 0,
+    offsetY: 0,
+  },
+  methods: {
+    onmousemove: function(e) {
+      this.screenX = e.screenX;
+      this.screenY = e.screenY;
+      this.pageX = e.pageX;
+      this.pageY = e.pageY;
+      this.clientX = e.clientX;
+      this.clientY = e.clientY;
+      this.offsetX = e.offsetX;
+      this.offsetY = e.offsetY;
+    }
+  }
+})
