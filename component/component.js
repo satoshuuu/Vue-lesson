722c7941d39377fb0ae64de4d2a3ba8977116d13
佐藤周平 <s.s.odn.0617@gmail.com>
2020-06-26 20:58:31 +0900

    add dynamic component

--- component/component.js
+++ component/component.js
@@ -142,4 +142,105 @@ Vue.component('my-book', {
 });
 new Vue({
   el: "#app8",
-})
+});
+
+//動的コンポーネントの基本
+Vue.component('banner-morning', {
+  template: `<div class="banner">
+    <h3>おはようございます！</h3>
+    <p>今日も1日頑張りましょう。</p>
+  </div>`
+});
+Vue.component('banner-noon', {
+  template: `<div class="banner">
+    <h3>こんにちは！</h3>
+    <p>お疲れ様です！午後も頑張りましょう！</p>
+  </div>`
+});
+Vue.component('banner-afternoon', {
+  template: `<div class="banner">
+    <h3>こんばんは！</h3>
+    <p>今日も1日お疲れ様でした！</p>
+  </div>`
+});
+new Vue({
+  el: '#app9',
+  created: function() {
+    let that = this;
+    this.interval = setInterval(function() {
+      that.current = (that.current + 1) % that.components.length;
+    }, 3000);
+  },
+  beforeDestroy: function() {
+    clearInterval(this.interval);
+  },
+  computed: {
+    currentBanner: function() {
+      return 'banner-' + this.components[this.current];
+    }
+  },
+  data: {
+    current: 0,
+    components: ['morning', 'noon', 'afternoon']
+  }
+});
+
+Vue.component('tab-member', {
+  template: `<div class="tab">
+    <h3>アルバイト募集中！</h3>
+    <p>Cat Cafeで一緒にお仕事しませんか？</p>
+    <label>名前：<input type="text" v-model="name"></label>
+    <input type="submit" value="登録">
+  </div>`,
+  data: function() {
+    return {
+      name: ''
+    }
+  }
+});
+Vue.component('tab-salary', {
+  template: `<div class="tab">
+    <h3>給料</h3>
+    <p>月~金 950円<br>土・日・祝日 1050円</p>
+  </div>`,
+  data: function() {
+    return {
+      name: ''
+    }
+  }
+});
+Vue.component('tab-env', {
+  template: `<div class="tab">
+    <h3>職場環境</h3>
+    <p>優しい人やかわいい猫に囲まれた、楽しい職場です！</p>
+  </div>`,
+  data: function() {
+    return {
+      name: ''
+    }
+  }
+});
+new Vue({
+  el: "#app10",
+  methods: {
+    onclick: function(tab) {
+      this.current = tab;
+    }
+  },
+  computed: {
+    tabNames: function() {
+      return Object.keys(this.tabs);
+    },
+    currentTab: function() {
+      return 'tab-' + this.current;
+    }
+  },
+  data: {
+    current: 'member',
+    tabs: {
+      'member': 'アルバイト募集',
+      'salary': '給料',
+      'env': '職場環境',
+    }
+  }
+});
